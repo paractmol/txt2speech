50f79e6d968329f9d872b444f7c6b885da2dac57
rudkovsky <rudkovsky@ymail.com>
2014-12-13 14:58:32 +0200

    fixing logic to have correct punctuation

--- lib/txt2speech/txt2speech.rb
+++ lib/txt2speech/txt2speech.rb
@@ -14,21 +14,19 @@ class Txt2Speech
  
 	def self.load(file_path, lang = 'en')
 		f = File.open(file_path)
-		self.new f.read.force_encoding(Encoding::UTF_8), lang
+		self.new f.read.encode("UTF-16be", :invalid=>:replace, :replace=>"?").encode('UTF-8')
 	end
  
 	def save(file_path)
 		uri = URI(GOOGLE_TRANSLATE_URL)
- 
-		attempts = text.length / 99.0
-		starts = 0
-		ar = response = []
- 
-		attempts.ceil.times do 
-			ends = starts + 99
-			ar << text[starts...ends]
-			starts = ends
-		end
+		
+		response = []
+		ar = text.split(/[,.\r\n]/i)
+		ar.reject! {|t| t.empty? }
+		ar.map! {|t| divide(t) }.flatten!
+		ar.map! {|t| t.strip }
+
+		puts ar.inspect
  
 		ar.each do |q|
 			uri.query = URI.encode_www_form({ ie: 'UTF-8', q: q, tl: lang, total: 1, idx: 5, textlen: q.length, prev: 'input'  })
@@ -41,5 +39,23 @@ class Txt2Speech
 			f.write response.join
 			return f.path
 		end
+	end
+
+	private
+
+	def divide(text)
+		return text if text.length < 99
+
+		attempts = text.length / 99.0
+		starts = 0
+		ar = []
+
+		attempts.ceil.times do 
+			ends = starts + 99
+			ar << text[starts...ends]
+			starts = ends
+		end
+
+		ar
 	end
 end
